1456b36f1ae05411456a47fa20e7e1cff554c73a
yewn2 <yewn2@middleton.school.nz>
2025-05-30 22:43:14 +1200

    Final commit of assembled game

---
+++ 00_CAR_RACING_GAME.py
@@ -0,0 +1,42 @@
+"""
+This is the final fully assembled version of my Car Racing Game.
+"""
+
+
+# imports
+import pygame
+import os
+import menu_FINAL as menu
+
+# size constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+pygame.init()
+
+# setting up the game window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+icon_path = os.path.join("Assets", "game_icon.png")
+game_icon = pygame.image.load(icon_path)
+pygame.display.set_icon(game_icon)
+
+# colours
+grey = (166, 166, 166)
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+# fonts
+score_font_path = os.path.join("Fonts", "Pixelify_Sans", "PixelifySans-VariableFont_wght.ttf")
+score_font = pygame.font.SysFont(os.path.abspath(score_font_path), 25)
+msg_font_path = os.path.join("assets", "Press_Start_2P", "PressStart2P-Regular.ttf")
+msg_font = pygame.font.SysFont(score_font_path, 75)
+exit_font = pygame.font.SysFont("freesansbold.ttf", 100, bold=True)
+
+
+def car_racing_game():
+    menu.menu_setup()
+
+
+# Main routine
+if __name__ == "__main__":
+    car_racing_game()
